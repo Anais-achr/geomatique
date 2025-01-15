d64a400bfd1c0e39277759a6f9673f8fafb00758
Anais-achr <anaisaoucher48@gmail.com>
2025-01-15 08:51:27 +0100

    commit

---
+++ utils.js
@@ -0,0 +1,29 @@
+const jourSemaine = ["Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"];
+
+function trieJourSemaine(tab){
+    tab.sort(([keyA, horairesA], [keyB, horairesB]) => {
+        const indexA = jourSemaine.indexOf(horairesA.jour);
+        const indexB = jourSemaine.indexOf(horairesB.jour);
+
+        return indexA - indexB;
+    })
+
+    return tab
+}
+
+/* function regrouperHorairesParJour(tab) {
+    let horairesParJour = tab.reduce((acc, current) => {
+        if (acc[current.jour_de_la_semaine]) {
+            acc[current.jour_de_la_semaine].push(current.horaire_en_jour_normal);
+        } else {
+            acc[current.jour_de_la_semaine] = [current.horaire_en_jour_normal];
+        }
+        return acc; // Retourne l'accumulateur à chaque itération
+    }, {}); // Initialise l'accumulateur comme un objet vide
+
+    return Object.entries(horairesParJour).map(([jour, horaires]) => ({
+        jour_de_la_semaine: jour,
+        horaires: horaires.join(", "),
+    }));
+}
+ */
