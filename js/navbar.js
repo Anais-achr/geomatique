daa1f8e5bf8f2a0e7fda78dbad85638a545f5e5b
ihssane20 <ihssanebennani7@gmail.com>
2025-01-19 19:33:32 +0100

    add home page

---
+++ js/navbar.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+    fetch("navbar.html")
+      .then(response => response.text())
+      .then(html => {
+        document.getElementById("navbar").innerHTML = html;
+      })
+      .catch(error => console.error("Erreur lors du chargement de la barre de navigation :", error));
+  });
+  
