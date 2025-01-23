e3104e1d9ea83d1d89de1694601d6deced4abece
Anais-achr <anaisaoucher48@gmail.com>
2025-01-24 00:50:38 +0100

    navbar

---
+++ js/navbar.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+    fetch("navbar.html")
+      .then(response => response.text())
+      .then(html => {
+        document.getElementById("navbar").innerHTML = html;
+      })
+      .catch(error => console.error("Erreur lors du chargement de la barre de navigation :", error));
+  });
